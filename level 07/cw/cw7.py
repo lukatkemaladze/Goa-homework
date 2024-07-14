72b3edef20dccaf65d6c563175288c885d1a61d2
lukatkemaladze <lukatkemaladze5@gmail.com>
2024-07-14 14:22:59 +0400

    07.14

---
+++ level 07/cw/cw7.py
@@ -0,0 +1,11 @@
+best_food = input("which food do you like")
+print(best_food)
+
+name = input("enter your name:")
+surname = input ("enter your surname")
+print(name + surname)
+
+num1 = input ("enter number")
+num2 = input ("enter number")
+print(num1+num2)
+
---
+++ level 07/hw/hw7.py
@@ -0,0 +1,18 @@
+#input არის ფუნქცია
+#ფუნქციებს ყოველთვის ბოლოში აქვთ ()
+#მაგ:
+#input("enter your fav.food")
+#I/O ასე იწერება input/output
+#input-ს შეაქვს ინფორმაცია,ხოლო output-ს-გამოაქვს
+#output-იც ფუნქციაა,მაგრამ განსხვავდებიან ერთმანეთისგან
+
+#string-ები როცა ერთებიან მას ეწოდება კონკატენაცია
+
+#name = input("< enter your name")
+#surname = input("< enter your surname")
+#print(name + surname)
+
+eating_what_i_want = input("what do you want to eat?")
+print(eating_what_i_want)
+
+
