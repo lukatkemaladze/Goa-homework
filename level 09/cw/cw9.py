1cfe9f64128448b142b15b4a93dd988e85fdb851
lukatkemaladze <lukatkemaladze5@gmail.com>
2024-07-22 12:42:19 +0400

    07.22

---
+++ level 09/cw/cw9.py
@@ -0,0 +1,3 @@
+how_old_are_you = int() (input)("how old are you :")
+print(how_old_are_you + 10)
+
---
+++ level 09/hw/hw9.py
@@ -0,0 +1,24 @@
+#age = 10
+#print(type(age))
+#name = "luka"
+#print(type(name))
+#height = 146.3
+#print(type(height))
+
+#number = input(5 + 7 - 6 * 9 / 7)
+#print(number)
+
+# / ამ გაყოფას გამოაქვს პასუხი float
+#num1 = 80 / 2
+#print(num1)
+# მაგრამ თუ გამოვიყენებთ ამ გაყოფას // გამოგვიტანს integer-ს
+#num2 = 80 // 2
+#print(num2)
+
+# type ფუნქცია გვაძლევს საშუალებას ვნახოთ თუ რა სახისაა ჩვენი დაწერილი კოდები
+
+#string-ები როცა ერთებიან ამ მოქმედებას ეწოდება კონკატენაცია
+
+კოღო = input("how_many_times_tried_to_bite_you:")
+მოგერიება = input("how many times did you repel it:")
+print(60 - 47)
