a485e40a5dbb1ae6e8d9b003eed77249cd95193b
lukatkemaladze <lukatkemaladze5@gmail.com>
2024-07-24 13:59:02 +0400

    07.24

---
+++ level 10/hw/hw10.py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ქვემო ქართლი</title>
+</head>
+<body>
+    <h1>ქვემო ქართლი</h1>
+    <hr>
+    <img src="" alt="ქვემო ქართლი">
+    <button></button>
+    <hr>
+    <p></p>
+    <hr>
+    <p></p> 
+    <hr>
+    <h3></h3>
+    <hr>
+    <h2></h2>
+    <p></p>
+    <hr>
+    <h2></h2>
+    <hr>
+    <p></p>
+    <p>@luka</p>
+</body>
+</html>
